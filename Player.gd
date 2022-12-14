58fdca71a554a5d4815197fb730a24d09954c9c0
Tale152 <alessandro.talmi@gmail.com>
2022-12-14 21:32:34 +0100

    implemented sprite flip on walk

--- Player.gd
+++ Player.gd
@@ -30,3 +30,11 @@ func _process(delta):
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
+	
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = velocity.y > 0
